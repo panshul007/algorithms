7e8a2abee1ccb4328dd73fa7f200bbbd0abea3ee
panshul <guptapanshul@gmail.com>
2015-03-02 03:58:08 +0100

    linkedlists

---
+++ src/main/java/panshul/learning/datastructures/linkedlist/LinkedList.java
@@ -0,0 +1,43 @@
+package panshul.learning.datastructures.linkedlist;
+
+public class LinkedList
+{
+    private Node head;
+
+    public void insertAtHead(int data)
+    {
+        Node newNode = new Node(data, this.head);
+        this.head = newNode;
+    }
+
+    public void deleteFromHead()
+    {
+        this.head = this.head.getNextNode();
+    }
+
+    public int length()
+    {
+        int length = 0;
+        Node current = this.head;
+
+        while (current != null)
+        {
+            length++;
+            current = current.getNextNode();
+        }
+        return length;
+    }
+
+    public String toString()
+    {
+        String result = "{";
+        Node current = this.head;
+        while (current != null)
+        {
+            result += current.toString() + ",";
+            current = current.getNextNode();
+        }
+        result += "}";
+        return result;
+    }
+}
---
+++ src/main/java/panshul/learning/datastructures/linkedlist/Node.java
@@ -0,0 +1,38 @@
+package panshul.learning.datastructures.linkedlist;
+
+public class Node
+{
+    private int  data;
+    private Node nextNode;
+
+    public Node(int data, Node nextNode)
+    {
+        this.data = data;
+        this.nextNode = nextNode;
+    }
+
+    public int getData()
+    {
+        return data;
+    }
+
+    public void setData(int data)
+    {
+        this.data = data;
+    }
+
+    public Node getNextNode()
+    {
+        return nextNode;
+    }
+
+    public void setNextNode(Node nextNode)
+    {
+        this.nextNode = nextNode;
+    }
+
+    public String toString()
+    {
+        return "Data: " + this.data;
+    }
+}
