d0fc672e84dd9c310745f3d9bc25c7d4163baefd
panshul <guptapanshul@gmail.com>
2015-03-22 00:12:14 +0100

    multi threading- blocking queue documented

--- src/main/java/panshul/learning/multithreading/blockingqueue/App.java
+++ src/main/java/panshul/learning/multithreading/blockingqueue/App.java
@@ -6,6 +6,11 @@ import java.util.concurrent.BlockingQueue;
 
 /**
  * Demonstrating producer consumer pattern.
+ * A BlockingQueue is used, it is thread safe queue.
+ * The producer is pushing elements into the queue. If queue is full, it will wait till there is
+ * space to put in more elements.
+ * The consumer is taking elements form the queue at random intervals. If the queue is empty it
+ * will wait for more elements to be pushed into the queue.
  */
 public class App
 {
@@ -48,7 +53,7 @@ public class App
         t1.start();
         t2.start();
 
-        t1.join();
+        t1.join(); //waits for the thread to complete its task and then terminate.
         t2.join();
     }
 
